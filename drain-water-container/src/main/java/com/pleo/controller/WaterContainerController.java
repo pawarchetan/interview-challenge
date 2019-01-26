509b245c790b31d10001de75ba46104afc3c0120
chetan.pawar <chetan.pawar@myob.com>
2019-01-27 01:34:58 +1100

    swagger api documentation

--- drain-water-container/src/main/java/com/pleo/controller/WaterContainerController.java
+++ drain-water-container/src/main/java/com/pleo/controller/WaterContainerController.java
@@ -2,6 +2,8 @@ package com.pleo.controller;
 
 import com.pleo.model.StructureDto;
 import com.pleo.service.WaterContainerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/watercontainer/api/v1")
+@Api(description = "Operation to return/get units of water contained in container formed by cells.")
 public class WaterContainerController {
 
     private final WaterContainerService waterContainerService;
@@ -19,7 +22,8 @@ public class WaterContainerController {
         this.waterContainerService = waterContainerService;
     }
 
-    @GetMapping("/units")
+    @GetMapping(value = "/units", produces = "application/json")
+    @ApiOperation(value = "Get units of water contained in container formed by cells.", response = StructureDto.class)
     public StructureDto getUnitsOfWaterContained(@RequestParam(value = "height") int[] cellHeights) {
         return waterContainerService.getUnitsOfWaterContained(cellHeights);
     }
