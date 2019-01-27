aee75e0e94e5ebbee640fb51797c4e088494ea50
chetan.pawar <chetan.pawar@myob.com>
2019-01-28 02:45:34 +1100

    log exception details to console

--- drain-water-container/src/main/java/com/pleo/controller/DefaultControllerAdvice.java
+++ drain-water-container/src/main/java/com/pleo/controller/DefaultControllerAdvice.java
@@ -2,6 +2,7 @@ package com.pleo.controller;
 
 import com.pleo.exception.InvalidCellHeightsException;
 import com.pleo.model.WaterContainerError;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
+@Log4j2
 public class DefaultControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(InvalidCellHeightsException.class)
     public ResponseEntity<WaterContainerError> handleInvalidCellHeightsException(InvalidCellHeightsException ex) {
+        log.error("Exception occurred : " + ex);
         WaterContainerError waterContainerError = new WaterContainerError();
         waterContainerError.setMessage(ex.getMessage());
         waterContainerError.setStatusCode(HttpStatus.BAD_REQUEST.value());
